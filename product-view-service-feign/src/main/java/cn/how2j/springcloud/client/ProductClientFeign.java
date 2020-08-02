388b35005056719fbab06b1611e71f624fc45b22
Lenovo <https://github.com/rouwan-wu/springcloud.git>
2020-08-02 18:33:50 +0800

    test

--- product-view-service-feign/src/main/java/cn/how2j/springcloud/client/ProductClientFeign.java
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/client/ProductClientFeign.java
@@ -1,12 +1,13 @@
 package cn.how2j.springcloud.client;
 
-import cn.how2j.springcloud.pojo.Product;
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import cn.how2j.springcloud.pojo.Product;
 
-@FeignClient(value = "PRODUCT-DATA-SERVICE")
+@FeignClient(value = "PRODUCT-DATA-SERVICE",fallback = ProductClientFeignHystrix.class)
 public interface ProductClientFeign {
 
     @GetMapping("/products")
